3cd20f6aaf4ac55a4e99a43128741e52e3208163
natesoul <natesoul@users.noreply.github.com>
2020-01-29 20:33:00 +0900

    finish 1

--- 1/src/index.js
+++ 1/src/index.js
@@ -1,5 +1,5 @@
 // <⚠️ DONT DELETE THIS ⚠️>
-import "./styles.css";
+// import "./styles.css";
 const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
 // <⚠️ /DONT DELETE THIS ⚠️>
 
@@ -12,4 +12,38 @@ const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
 ✅ DO NOT CHANGE .css, or .html files.
 ✅ ALL function handlers should be INSIDE of "superEventHandler"
 */
-const superEventHandler = {};
+const superEventHandler = {
+  over:"mouseover",
+  leave:"mouseleave",
+  resize:"resize",
+  right:"auxclick"
+};
+
+const title = document.querySelector("h2");
+
+
+
+function handleOver(){
+  title.style.color = colors [1];
+  title.innerHTML = "The mouse is here!";
+}
+
+function handleLeave(){
+  title.style.color = colors [0];
+  title.innerHTML = "The mouse is gone!";
+}
+
+function handleResize(){
+  title.style.color = colors [2];
+  title.innerHTML = "You just resized!";
+}
+
+function handleRight(){
+  title.style.color = colors [4];
+  title.innerHTML = "That was a right click!";
+}
+
+title.addEventListener(superEventHandler.over,handleOver);
+title.addEventListener(superEventHandler.leave, handleLeave);
+window.addEventListener(superEventHandler.resize, handleResize);
+document.addEventListener(superEventHandler.right, handleRight);
