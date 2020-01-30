6364662ae88ac4b8d9dd73cffce492ca500f9538
natesoul <59507324+natesoul@users.noreply.github.com>
2020-01-30 12:14:06 +0900

    2 start

---
+++ 2/src/index.js
@@ -0,0 +1,11 @@
+// <⚠️ DONT DELETE THIS ⚠️>
+import "./styles.css";
+// <⚠️ /DONT DELETE THIS ⚠️>
+
+
+const fontColor = "#ffffff";
+
+
+title = document.querySelector(".title");
+
+console.log("title");
