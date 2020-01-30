5247639e207b6bcc0da3d85b9349fe771a73f553
natesoul <natesoul@users.noreply.github.com>
2020-01-30 21:40:42 +0900

    finish 2

--- 2/src/index.js
+++ 2/src/index.js
@@ -2,15 +2,23 @@
 // import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
 
-const colors = ["#ffffff", "#FF00FF", "#FFD700", "#00BFFF"];
-
-
-
-title = document.querySelector(".title");
-body = document.querySelector("body");
+const colors = ["#ffffff", "#9932CC", "#FFD700", "#00BFFF"];
+const title = document.querySelector("h1");
+const body = document.querySelector("body");
 
 
 body.style.backgroundColor = colors[1];
 title.style.color = colors[0];
 
-console.log(body);
+function handleResize (){
+    let viewportWidth = window.innerWidth;
+    if (viewportWidth > 620) {
+        body.style.backgroundColor = colors[2];
+    } else if(viewportWidth < 470 ) {
+        body.style.backgroundColor = colors[3];
+    } else {
+        body.style.backgroundColor = colors[1];
+    }
+}
+
+window.addEventListener("resize", handleResize);
