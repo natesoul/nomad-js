ea42a2eb8370a327ff615adca5bebf16bf798729
natesoul <59507324+natesoul@users.noreply.github.com>
2020-01-31 20:16:44 +0900

    3-2

--- 3/src/index.js
+++ 3/src/index.js
@@ -5,8 +5,35 @@ const NINE_HOURS_MILLISECONDS = 32400000;
 
 function getTime() {
   // Don't delete this.
-  const xmasDay = new Date("2019-12-24:00:00:00+0900");
+
+  const date = new Date("2020-03-31:10:30:00+0900");
+  
+  const xmasDay = new Date("2020-01-31:16:33:00+0900");
+
+  const dday = date.getTime() - xmasDay.getTime();
+
+  const seconds = dday / 1000;
+  const minutes = seconds / 60; 
+  const hours = minutes / 60;
+  const days = hours / 24;
+  const dayss = int(days);
+  // const hourss = dayss * 
 }
 
-function init() {}
+
+
+
+
+
+function init() {
+  getTime();
+}
+
 init();
+
+console.log(dday);
+console.log(seconds);
+console.log(minutes);
+console.log(hours);
+console.log(days);
+console.log(dayss);
