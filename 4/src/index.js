ff74dfe0236eb35804b352deae63485a79a3940e
natesoul <natesoul@users.noreply.github.com>
2020-02-01 19:32:31 +0900

    4 mid

--- 4/src/index.js
+++ 4/src/index.js
@@ -1,3 +1,31 @@
 // <⚠️ DONT DELETE THIS ⚠️>
-import "./styles.css";
+// import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
+
+const myCountry = document.querySelector(".myCountry");
+
+function select(event) {
+    event.preventDefault();
+    const currentValue = myCountry.value;
+    saveCountry(currentValue);
+}
+
+function saveCountry(text) {
+    myCountry.addEventListener("change", select);
+    localStorage.setItem("Country", text);
+}
+
+// function askForCountry() {
+//     myCountry.addEventListener("onselect", select);
+// }
+
+function askForCountry(){
+    myCountry.addEventListener("change", select);
+}
+
+function init() {
+    
+    askForCountry();
+}
+
+init();
