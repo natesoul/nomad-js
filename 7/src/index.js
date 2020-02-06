9747477aad9f39280d1b2e183d0b1aaec89ec188
natesoul <59507324+natesoul@users.noreply.github.com>
2020-02-06 19:22:47 +0900

    7 finish2

--- 7/src/index.js
+++ 7/src/index.js
@@ -3,12 +3,10 @@
 // <⚠️ /DONT DELETE THIS ⚠️>
 
 let total = 0;
-let midTotal = 0;
 let cal ="";
 let cal2 ="";
 const body = document.querySelector("body");
 const result = document.querySelector(".result");
-let resultDisplay = result.innerText;
 let calculators = "";
 
 function input (event) {
@@ -19,49 +17,36 @@ function input (event) {
             cal2="";
             calculators="";
             total = 0;
-            result.innerText = cal;        
+            result.innerText = "";      
         } else if (content == "=") {
-            console.log("=");
-            console.log(calculators);
             calculation();
             cal2="";
 
         } else if (content =="+" || content =="-" || 
         content =="*" || content =="/") {
             if (cal2 !== "") {
-                console.log(cal2, calculators);
                 calculation(); 
-                calculators = content;       
+                calculators = content;      
             } else if (cal2 == "") {
                 calculators = content;
                 cal2 = cal;
                 cal ="";
-                console.log(cal2, calculators);
-         
-            
             } else {
                 calculators = content;
-                console.log(calculators);
                 cal2 = cal;
                 cal = "";
             }
         }
         else {
             const sContent = String(content);
-            console.log(sContent);
             cal = cal + sContent;
             result.innerText = cal;
        }
     }
 }
 
-
-
 function calculation () {
-    console.log(calculators);
-    console.log("cal2", cal2, "cal", cal);
     switch (calculators) {
-        
         case "+":
             total = Number(cal2) + Number(cal);
             resultNumber();
